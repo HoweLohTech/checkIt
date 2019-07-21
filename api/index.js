2fbec2bb3232528a483f547b2173ee171759fe17
Eric Ed Lohmar <ramhol.cire@gmail.com>
2019-07-21 13:27:35 -0500

    Added flights data get request

--- api/index.js
+++ api/index.js
@@ -11,5 +11,15 @@ const url = `mongodb+srv://${credString}@cluster0-mhfkr.gcp.mongodb.net/`
 const getClient = () => new MongoClient(url, { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
-    const client = getClient
+    const client = getClient()
+    client.connect((err) =>{
+        client.db('data')
+            .collection('flights')
+            .find({})
+            .toArray()
+            .then((data) => res.json(data))
+            .then(client.close())
+    })
 })
+
+app.listen(3000)
