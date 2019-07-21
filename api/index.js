c1b29c422bb336ef28d20f27e0336601b6e0c9b8
Eric Ed Lohmar <ramhol.cire@gmail.com>
2019-07-21 12:36:40 -0500

    Mistype correction

--- api/index.js
+++ api/index.js
@@ -6,4 +6,4 @@ const credString = `${creds.UID}:${encodeURIComponent(creds.PWD)}`
 const url = `mongodb+srv://${credString}@cluster0-mhfkr.gcp.mongodb.net/`
 
 // Connect using MongoClient
-const getClinet = () => new MongoClient(url, { useNewUrlParser: true });
+const getClient = () => new MongoClient(url, { useNewUrlParser: true });
